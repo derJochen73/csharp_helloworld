61ad128b5cc4bcaa87db13034e699f177e87cbc9
jk <kohler.jochen@gmail.com>
2020-03-31 18:12:54 +0200

    First test

---
+++ github_console_learning/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github_console_learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Masterbranch");
+            Console.ReadKey();
+        }
+    }
+}
